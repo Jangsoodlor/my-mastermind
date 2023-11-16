22d3e064927ae473ab429179353277358a592da3
Rattapoom Pethputong <celica2548@gmail.com>
2023-11-16 10:36:42 +0700

    Add main part, the game is now playable.

--- mastermind.py
+++ mastermind.py
@@ -101,7 +101,41 @@ class Main:
 
 
 if __name__ == '__main__':
+    print("Mastermind, a game to kill your time\n\
+Copyright (C) <2023>  <Jangsoodlor>\n\
+\n\
+This program is free software: you can redistribute it and/or modify\n\
+it under the terms of the GNU General Public License as published by\n\
+the Free Software Foundation, either version 3 of the License, or\n\
+(at your option) any later version.\n\
+\n\
+This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+GNU General Public License for more details.\n\
+\n\
+You should have received a copy of the GNU General Public License\n\
+along with this program.  If not, see <https://www.gnu.org/licenses/>.\n")
+    end_game = False
     game = Main()
-    game.color = 5
-    game.position = 3
-    game.play()
+    while end_game == False:
+        print('Game Options')
+        print('1. Start the game')
+        print('2. Set puzzle length')
+        print('3. Set the amount of colors')
+        print('4. How to Play?')
+        print('5. Quit')
+        option = int(input('enter your option: '))
+        if option == 1:
+            game.play()
+            play_again = input('Play Again? (Y/N): ')
+            if play_again.upper() == 'N':
+                end_game = True
+        elif option == 2:
+            game.position = int(input('Set the puzzle length form 1 to 10: '))
+        elif option == 3:
+            game.color = int(input('Set the amount of colors from 1 to 8: '))
+        elif option == 4:
+            print('Ask ChatGPT')
+        elif option == 5:
+            end_game = True
