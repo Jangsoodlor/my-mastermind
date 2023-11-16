910444df0ac524943601cbacd44ca3b5ee96a5e9
Rattapoom Pethputong <celica2548@gmail.com>
2023-11-16 10:07:58 +0700

    checked that all features works correctly

--- mastermind.py
+++ mastermind.py
@@ -1,31 +1,32 @@
-# import random
+import random
 import copy
 class Game:
     def __init__(self, pos, color) -> None:
         self.__pos = pos
         self.__color = color
-        self.__answer = ['3', '3', '3', '6']
-        # for i in range(pos):
-        #     self.__answer.append(random.randint(1, color))
-
+        self.__answer = []
+        for _ in range(pos):
+            self.__answer.append(str(random.randint(1, color)))
 
     @property
     def guess(self):
         return ''.join(self.__guess)
 
-
     @guess.setter
     def guess(self, value):
         self.__guess = list(value)
 
-
+    @property
+    def answer(self):
+        return ''.join(self.__answer)
+    
     @property
     def give_hint(self):
         hint = []
         pop_list = []
         if len(self.__guess) != len(self.__answer):
-            raise AssertionError('Are you an idiot?')
-
+            raise AssertionError\
+                ('Your guess must be the same length as your puzzle length, baka.')
         guess_temp = copy.deepcopy(self.__guess)
         answer_temp = copy.deepcopy(self.__answer)
 
@@ -33,21 +34,18 @@ class Game:
             if guess_temp[i] == answer_temp[i]:
                 hint.append('*')
                 pop_list.append(i)
-
         pop_list.sort()
         pop_list.reverse()
 
         for i in pop_list:
             guess_temp.pop(i)
             answer_temp.pop(i)
-
         for i in range(len(guess_temp)):
             if guess_temp[i] in answer_temp:
                 hint.append('o')
                 answer_temp.remove(guess_temp[i])
 
         return ''.join(hint)
-
 
     def __str__(self) -> str:
         return f'Playing Mastermind with {self.__color} colors and {self.__pos} positions'
@@ -66,7 +64,7 @@ class Main:
     @position.setter
     def position(self, pos):
         self.__pos = pos
-        if 1 < self.__pos < 10:
+        if 1 > self.__pos > 10:
             raise ValueError('the puzzle length must be from 1 - 10')
         self.__reset()
 
@@ -77,10 +75,9 @@ class Main:
     @color.setter
     def color(self, color):
         self.__color = color
-        if 1 < self.__color < 8:
-            raise ValueError('You can only have 8 colors')
+        if 1 > self.__color > 8:
+            raise ValueError('You can only have 1-8')
         self.__reset()
-
 
     def __reset(self):
         self.__game = Game(self.__pos, self.__color)
@@ -92,15 +89,19 @@ class Main:
         while True:
             turn_count += 1
             self.__game.guess = input('Enter your guess: ')
+            if self.__game.guess == 'giveup':
+                print('You gave up')
+                print(f"The answer is {self.__game.answer}")
+                print(f"You've played for {turn_count} turn")
+                break
             print(self.__game.give_hint)
             if self.__game.give_hint == ('*' * self.__pos):
                 print(f"You successfully solved the puzzle in {turn_count} turns")
-                break
-            if self.__game.guess == 'giveup':
-                print('You gave up')
                 break
 
 
 if __name__ == '__main__':
     game = Main()
+    game.color = 5
+    game.position = 3
     game.play()
